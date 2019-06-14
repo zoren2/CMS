857936969ddfed701a5001846170f1a7e14e87ca
Jack Tsang <cic_tian2@yahoo.com>
2019-06-13 20:49:06 -0700

    Added select2 javascript plugin to clean up layout
    Beautifies the <select> HTML portion of the UI.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -73,7 +73,7 @@
                 @if($tags->count() > 0)
                     <div class="form-group">
                         <label for="tags">Tags</label>
-                        <select name="tags[]" id="tags" class="form-control" multiple>
+                        <select name="tags[]" id="tags" class="form-control tags-selector" multiple>
                             @foreach($tags as $tag)
                                 <option value="{{$tag->id}}"
                                         @if( isset($post) )
@@ -100,9 +100,15 @@
 @endsection
 
 @section('scripts')
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.1/trix.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script>
+        $(document).ready(function () {
+            $('.tags-selector').select2();
+        });
         flatpickr('#published_at', {
             enableTime: true
         });
@@ -110,6 +116,7 @@
 @endsection
 
 @section('css')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.1/trix.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" rel="stylesheet">
 @endsection
