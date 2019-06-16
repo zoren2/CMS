17036765c9536aa20abe1baceaf1e2b6d0f4ec58
Jack Tsang <cic_tian2@yahoo.com>
2019-06-16 08:37:56 -0700

    Edited policy to allow administrator rights over the blog

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -19,7 +19,7 @@ class PostPolicy
      */
     public function update(User $user, Post $post)
     {
-        return $user->id === $post->user_id;
+        return ($user->id === $post->user_id || auth()->user()->isAdmin());
     }
 
 }
